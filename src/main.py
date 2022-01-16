f62c072e12ab7a32ed24d61659374d849ef6ad86
grebacz <gabrielrebacz@gmail.com>
2022-01-16 13:48:22 +0100

    simple experiment on mnist

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
+import copy
+
+from avalanche.training import JointTraining
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
 from avalanche.benchmarks.classic import SplitMNIST
-from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics,\
-    loss_metrics, timing_metrics, cpu_usage_metrics, StreamConfusionMatrix,\
-    disk_usage_metrics, gpu_usage_metrics
+from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics, \
+    loss_metrics, timing_metrics, cpu_usage_metrics, confusion_matrix_metrics, disk_usage_metrics
 from avalanche.models import SimpleMLP
 from avalanche.logging import InteractiveLogger, TextLogger, TensorboardLogger
 from avalanche.training.plugins import EvaluationPlugin
-from avalanche.training.strategies import Naive
-
-from torch.optim import SGD
-from torch.nn import CrossEntropyLoss
+from avalanche.training.strategies import EWC
 
 scenario = SplitMNIST(n_experiences=5)
 
@@ -30,21 +31,18 @@ text_logger = TextLogger(open('log.txt', 'a'))
 interactive_logger = InteractiveLogger()
 
 eval_plugin = EvaluationPlugin(
-    accuracy_metrics(minibatch=True, epoch=True, experience=True, stream=True),
-    loss_metrics(minibatch=True, epoch=True, experience=True, stream=True),
-    timing_metrics(epoch=True),
-    cpu_usage_metrics(experience=True),
-    forgetting_metrics(experience=True, stream=True),
-    StreamConfusionMatrix(num_classes=scenario.n_classes, save_image=False),
-    disk_usage_metrics(minibatch=True, epoch=True, experience=True, stream=True),
+    accuracy_metrics(stream=True),
+    loss_metrics(stream=True),
+    forgetting_metrics(stream=True),
     loggers=[interactive_logger, text_logger, tb_logger]
 )
 
 # CREATE THE STRATEGY INSTANCE (NAIVE)
-cl_strategy = Naive(
+strategy = EWC(
     model, SGD(model.parameters(), lr=0.001, momentum=0.9),
-    CrossEntropyLoss(), train_mb_size=500, train_epochs=1, eval_mb_size=100,
+    CrossEntropyLoss(), ewc_lambda=1, train_mb_size=500, train_epochs=1, eval_mb_size=100,
     evaluator=eval_plugin)
+
 
 # TRAINING LOOP
 print('Starting experiment...')
@@ -54,9 +52,22 @@ for experience in scenario.train_stream:
     print("Current Classes: ", experience.classes_in_this_experience)
 
     # train returns a dictionary which contains all the metric values
-    res = cl_strategy.train(experience, num_workers=4)
+    res = strategy.train(experience)
     print('Training completed')
 
     print('Computing accuracy on the whole test set')
-    # eval also returns a dictionary which contains all the metric values
-    results.append(cl_strategy.eval(scenario.test_stream, num_workers=4))
+    # test also returns a dictionary which contains all the metric values
+    results.append(strategy.eval(scenario.test_stream))
+
+
+freeze_model = copy.deepcopy(model)
+freeze_model.features.requires_grad_(False)
+strategy = JointTraining(
+    freeze_model, SGD(freeze_model.parameters(), lr=0.001, momentum=0.9),
+    CrossEntropyLoss(), train_mb_size=500, train_epochs=1, eval_mb_size=100,
+    evaluator=eval_plugin)
+print('JOINT TRAINING ONLY ON LAST LAYER')
+strategy.train(scenario.train_stream)
+
+print('EVAL ON JOINT TRAINING ONLY ON LAST LAYER')
+results.append(strategy.eval(scenario.test_stream))
