3395ce48bc001530c2cb6ac72532fc82520ff207
grebacz <gabrielrebacz@gmail.com>
2022-01-16 15:06:44 +0100

    base icarl on cifar100

--- src/main.py
+++ src/main.py
@@ -1,53 +1,45 @@
 import copy
-
-from avalanche.training import JointTraining
+import torch
+import wandb
+from avalanche.benchmarks import SplitCIFAR100
+from avalanche.training import JointTraining, Replay, ICaRL
 from torch.optim import SGD
 from torch.nn import CrossEntropyLoss
 from avalanche.benchmarks.classic import SplitMNIST
 from avalanche.evaluation.metrics import forgetting_metrics, accuracy_metrics, \
     loss_metrics, timing_metrics, cpu_usage_metrics, confusion_matrix_metrics, disk_usage_metrics
-from avalanche.models import SimpleMLP
-from avalanche.logging import InteractiveLogger, TextLogger, TensorboardLogger
+from avalanche.models import SimpleMLP, SimpleCNN, make_icarl_net
+from avalanche.logging import InteractiveLogger, TextLogger, TensorboardLogger, WandBLogger
 from avalanche.training.plugins import EvaluationPlugin
 from avalanche.training.strategies import EWC
 
-scenario = SplitMNIST(n_experiences=5)
-
-# MODEL CREATION
-model = SimpleMLP(num_classes=scenario.n_classes)
-
-# DEFINE THE EVALUATION PLUGIN and LOGGERS
-# The evaluation plugin manages the metrics computation.
-# It takes as argument a list of metrics, collectes their results and returns
-# them to the strategy it is attached to.
-
-# log to Tensorboard
-tb_logger = TensorboardLogger()
-
-# log to text file
-text_logger = TextLogger(open('log.txt', 'a'))
-
-# print to stdout
-interactive_logger = InteractiveLogger()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+benchmark = SplitCIFAR100(n_experiences=5)
+model = make_icarl_net(num_classes=benchmark.n_classes)
+wandb.login(key='1101c4f4f913f15059e7ac2b09a4a3a9e9ff2325')
 
 eval_plugin = EvaluationPlugin(
-    accuracy_metrics(stream=True),
-    loss_metrics(stream=True),
-    forgetting_metrics(stream=True),
-    loggers=[interactive_logger, text_logger, tb_logger]
+    accuracy_metrics(epoch=True, experience=True, stream=True),
+    loss_metrics(epoch=True, experience=True, stream=True),
+    benchmark=benchmark,
+    loggers=[
+        InteractiveLogger(),
+        TextLogger(open('log.txt', 'a')),
+        TensorboardLogger(),
+        WandBLogger(project_name="reg-alg-cl-last-layer-importance", run_name="test-icarl")]
 )
 
-# CREATE THE STRATEGY INSTANCE (NAIVE)
-strategy = EWC(
-    model, SGD(model.parameters(), lr=0.001, momentum=0.9),
-    CrossEntropyLoss(), ewc_lambda=1, train_mb_size=500, train_epochs=1, eval_mb_size=100,
-    evaluator=eval_plugin)
+strategy = ICaRL(
+    model.feature_extractor, model.classifier, SGD(model.parameters(), lr=0.001, momentum=0.9),
+    buffer_transform=None, memory_size=20, fixed_memory=False,
+    train_mb_size=128, train_epochs=60, eval_mb_size=128,
+    evaluator=eval_plugin, device=device)
 
 
 # TRAINING LOOP
 print('Starting experiment...')
 results = []
-for experience in scenario.train_stream:
+for experience in benchmark.train_stream:
     print("Start of experience: ", experience.current_experience)
     print("Current Classes: ", experience.classes_in_this_experience)
 
@@ -57,17 +49,17 @@ for experience in scenario.train_stream:
 
     print('Computing accuracy on the whole test set')
     # test also returns a dictionary which contains all the metric values
-    results.append(strategy.eval(scenario.test_stream))
+    results.append(strategy.eval(benchmark.test_stream))
 
-
-freeze_model = copy.deepcopy(model)
-freeze_model.features.requires_grad_(False)
-strategy = JointTraining(
-    freeze_model, SGD(freeze_model.parameters(), lr=0.001, momentum=0.9),
-    CrossEntropyLoss(), train_mb_size=500, train_epochs=1, eval_mb_size=100,
-    evaluator=eval_plugin)
-print('JOINT TRAINING ONLY ON LAST LAYER')
-strategy.train(scenario.train_stream)
-
-print('EVAL ON JOINT TRAINING ONLY ON LAST LAYER')
-results.append(strategy.eval(scenario.test_stream))
+#
+# freeze_model = copy.deepcopy(model)
+# freeze_model.features.requires_grad_(False)
+# strategy = JointTraining(
+#     freeze_model, SGD(freeze_model.parameters(), lr=0.001, momentum=0.9),
+#     CrossEntropyLoss(), train_mb_size=500, train_epochs=1, eval_mb_size=100,
+#     evaluator=eval_plugin)
+# print('JOINT TRAINING ONLY ON LAST LAYER')
+# strategy.train(benchmark.train_stream)
+#
+# print('EVAL ON JOINT TRAINING ONLY ON LAST LAYER')
+# results.append(strategy.eval(benchmark.test_stream))
