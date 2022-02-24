832a634616de13aa4b88820e82d5e32495c3c01e
grebacz <gabrielrebacz@gmail.com>
2022-02-24 19:39:54 +0100

    hyperparameters parsing, setting up the experiment

---
+++ src/experiment.py
@@ -0,0 +1,111 @@
+import copy
+
+import randomname
+import torch
+from avalanche.benchmarks import SplitCIFAR100, SplitMNIST, SplitCUB200
+from avalanche.evaluation.metrics import accuracy_metrics, loss_metrics
+from avalanche.logging import InteractiveLogger, TextLogger, WandBLogger
+from avalanche.models import make_icarl_net
+from avalanche.training import EWC, LwF, SynapticIntelligence, JointTraining
+from avalanche.training.plugins import EvaluationPlugin
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+
+
+class Experiment:
+    device = None
+    args = None
+    benchmark = None
+    model = None
+    eval_plugin = None
+    optimizer = None
+    scheduler = None
+    strategy = None
+    plugins = None
+
+    def __init__(self, args):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        self.args = args
+        self.benchmark = self.get_benchmark(args)
+        self.model = make_icarl_net(num_classes=self.benchmark.n_classes, n=args.experiences)
+
+        self.eval_plugin = EvaluationPlugin(
+            accuracy_metrics(epoch=True, experience=True, stream=True),
+            loss_metrics(epoch=True, experience=True, stream=True),
+            benchmark=self.benchmark, loggers=[
+                InteractiveLogger(),
+                TextLogger(open('log.txt', 'a')),
+                WandBLogger(project_name="reg-alg-cl-last-layer-importance",
+                            run_name=f"test-icarl:[{randomname.get_name()}]")]
+        )
+
+        self.optimizer = Adam(self.model.parameters(), lr=args.learning_rate)
+
+        # todo if needed
+        self.scheduler = None
+        # todo if needed
+        self.plugins = None
+
+        self.strategy = self.get_strategy(args)
+
+    # TRAINING LOOP
+    def run_experiment(self):
+        print('Starting experiment...')
+        results = []
+        for experience in self.benchmark.train_stream:
+            print("Start of experience: ", experience.current_experience)
+            print("Current Classes: ", experience.classes_in_this_experience)
+
+            # train returns a dictionary which contains all the metric values
+            res = self.strategy.train(experience)
+            print('Training completed')
+
+            print('Computing accuracy on the whole test set')
+            # test also returns a dictionary which contains all the metric values
+            results.append(self.strategy.eval(self.benchmark.test_stream))
+
+        freeze_model = copy.deepcopy(self.model)
+        freeze_model.features.requires_grad_(False)
+        strategy = JointTraining(self.model, self.optimizer, CrossEntropyLoss(),
+                                 train_mb_size=self.args.batch_size, eval_mb_size=self.args.batch_size,
+                                 train_epochs=self.args.epochs, evaluator=self.eval_plugin, device=self.device,
+                                 plugins=self.plugins)
+
+        print('JOINT TRAINING ONLY ON LAST LAYER')
+        strategy.train(self.benchmark.train_stream)
+
+        print('EVAL ON JOINT TRAINING ONLY ON LAST LAYER')
+        results.append(strategy.eval(self.benchmark.test_stream))
+
+    def get_benchmark(self, args):
+        if args.dataset == 'CIFAR100':
+            return SplitCIFAR100(n_experiences=args.experiences)
+        elif args.dataset == 'MNIST':
+            return SplitMNIST(n_experiences=args.experiences)
+        elif args.dataset == 'CUB200':
+            return SplitCUB200(n_experiences=args.experiences)
+
+    def get_strategy(self, args):
+        if args.strategy == 'EWC':
+            return EWC(self.model, self.optimizer, CrossEntropyLoss(),
+                       train_mb_size=args.batch_size, eval_mb_size=args.batch_size,
+                       train_epochs=args.epochs, evaluator=self.eval_plugin, device=self.device, plugins=self.plugins,
+                       ewc_lambda=args.ewc_lambda, mode=args.ewc_mode, decay_factor=args.ewc_decay_factor,
+                       keep_importance_data=args.ewc_keep_importance_data)
+        elif args.strategy == 'LWF':
+            return LwF(self.model, self.optimizer, CrossEntropyLoss(),
+                       train_mb_size=args.batch_size, eval_mb_size=args.batch_size,
+                       train_epochs=args.epochs, evaluator=self.eval_plugin, device=self.device, plugins=self.plugins,
+                       alpha=args.lwf_alpha, temperature=args.lwf_temperature)
+        elif args.strategy == 'SI':
+            return SynapticIntelligence(self.model, self.optimizer, CrossEntropyLoss(),
+                                        train_mb_size=args.batch_size, eval_mb_size=args.batch_size,
+                                        train_epochs=args.epochs, evaluator=self.eval_plugin, device=self.device,
+                                        plugins=self.plugins,
+                                        si_lambda=args.si_lambda, eps=args.si_eps)
+        elif args.strategy == 'JOINT':
+            return JointTraining(self.model, self.optimizer, CrossEntropyLoss(),
+                                 train_mb_size=args.batch_size, eval_mb_size=args.batch_size,
+                                 train_epochs=args.epochs, evaluator=self.eval_plugin, device=self.device,
+                                 plugins=self.plugins)
--- src/main.py
+++ src/main.py
@@ -1,66 +1,76 @@
-import randomname
-import torch
-from avalanche.benchmarks import SplitCIFAR100
-from avalanche.evaluation.metrics import accuracy_metrics, \
-    loss_metrics
-from avalanche.logging import InteractiveLogger, TextLogger, WandBLogger
-from avalanche.models import make_icarl_net
-from avalanche.training import ICaRL
-from avalanche.training.plugins import EvaluationPlugin, LRSchedulerPlugin
-from torch.optim import SGD, Adam
-from torch.optim.lr_scheduler import MultiStepLR
+import argparse
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(f'using device {device}')
-
-benchmark = SplitCIFAR100(n_experiences=5)
-model = make_icarl_net(num_classes=benchmark.n_classes, n=5)
-
-eval_plugin = EvaluationPlugin(
-    accuracy_metrics(epoch=True, experience=True, stream=True),
-    loss_metrics(epoch=True, experience=True, stream=True),
-    benchmark=benchmark, loggers=[
-        InteractiveLogger(),
-        TextLogger(open('log.txt', 'a')),
-        WandBLogger(project_name="reg-alg-cl-last-layer-importance", run_name=f"test-icarl:[{randomname.get_name()}]")]
-)
-
-optimizer = SGD(model.parameters(), lr=2, momentum=0.9)
-scheduler = MultiStepLR(optimizer, milestones=[49, 63], gamma=0.2)
-
-strategy = ICaRL(
-    model.feature_extractor, model.classifier, optimizer,
-    buffer_transform=None, memory_size=2000, fixed_memory=True,
-    train_mb_size=128, train_epochs=70, eval_mb_size=128,
-    evaluator=eval_plugin, device=device,
-    plugins=[LRSchedulerPlugin(scheduler)]
-)
+from experiment import Experiment
 
 
-# TRAINING LOOP
-print('Starting experiment...')
-results = []
-for experience in benchmark.train_stream:
-    print("Start of experience: ", experience.current_experience)
-    print("Current Classes: ", experience.classes_in_this_experience)
+def parse_parameters():
+    parser = argparse.ArgumentParser(description='This program is used to start one of the prepared experiments'
+                                                 'concerning studying the importance of last layer of neural network'
+                                                 'in the catastrophic forgetting during regularization based continual'
+                                                 'class learning strategy.')
+    parser.add_argument('--dataset', choices=['CIFAR100', 'MNIST', 'CUB200'], nargs='?', default='CIFAR100',
+                        help='dataset on which strategy will be used')
+    parser.add_argument('--epochs', type=int, help='number of epochs', default=80)
+    parser.add_argument('--experiences', type=int, help='number of even class splits', default=5)
+    parser.add_argument('--learning_rate', type=float, help='learning rate of Adam optimizer', default=0.001)
+    parser.add_argument('--batch_size', type=int,
+                        help='the train minibatch size', default=32)
 
-    # train returns a dictionary which contains all the metric values
-    res = strategy.train(experience)
-    print('Training completed')
+    subparsers = parser.add_subparsers(help='specific continual learning algorithm type', dest='strategy',
+                                       required=True, metavar='algorithm')
 
-    print('Computing accuracy on the whole test set')
-    # test also returns a dictionary which contains all the metric values
-    results.append(strategy.eval(benchmark.test_stream))
+    # EWC
+    parser_ewc = subparsers.add_parser('EWC', help='Elastic Weight Consolidation')
+    parser_ewc.add_argument('--ewc_lambda', type=float,
+                        help='EWC hyperparameter to weigh the penalty inside the total loss. The larger the lambda, '
+                             'the larger the regularization',
+                        default=0.1)
 
-#
-# freeze_model = copy.deepcopy(model)
-# freeze_model.features.requires_grad_(False)
-# strategy = JointTraining(
-#     freeze_model, SGD(freeze_model.parameters(), lr=0.001, momentum=0.9),
-#     CrossEntropyLoss(), train_mb_size=500, train_epochs=1, eval_mb_size=100,
-#     evaluator=eval_plugin)
-# print('JOINT TRAINING ONLY ON LAST LAYER')
-# strategy.train(benchmark.train_stream)
-#
-# print('EVAL ON JOINT TRAINING ONLY ON LAST LAYER')
-# results.append(strategy.eval(benchmark.test_stream))
+    parser_ewc.add_argument('--ewc_mode', choices=['separate, onlinesum'], nargs='?', default='separate',
+                        help='separate to keep a separate penalty for each previous experience. onlinesum to keep a '
+                             'single penalty summed over all previous tasks. onlineweightedsum to keep a single '
+                             'penalty summed with a decay factor over all previous tasks')
+    parser_ewc.add_argument('--ewc_decay_factor', type=float, default=None,
+                        help='used only if mode is onlineweightedsum. It specify the decay term of the importance '
+                             'matrix')
+    parser_ewc.add_argument('--ewc_keep_importance_data', type=bool, default=True,
+                        help='if True, keep in memory both parameter values and importances for all previous task, '
+                             'for all modes. If False, keep only last parameter values and importances. If mode is '
+                             'separate, the value of keep_importance_data is set to be True.')
+
+    # LWF
+    parser_lwf = subparsers.add_parser('LWF', help='Learning without Forgetting')
+
+    parser_lwf.add_argument('--lwf_alpha', type=float,
+                        help='LwF distillation hyperparameter. It can be either a float number or a list containing '
+                             'alpha for each experience.',
+                        default=0.2)
+    parser_lwf.add_argument('--lwf_temperature', type=float, help='LwF softmax temperature for distillation', default=3)
+
+    # SI
+    parser_si = subparsers.add_parser('SI', help='Synaptic Intelligence')
+
+    parser_si.add_argument('--si_lambda', type=float,
+                        help='Synaptic Intelligence lambda term. If list, one lambda for each experience. If the list '
+                             'has less elements than the number of experiences, last lambda will be used for the '
+                             'remaining experiences', default=3)
+    parser_si.add_argument('--si_eps', type=float,
+                        help='Synaptic Intelligence damping parameter.', default=1e-07)
+
+    # JOINT
+    parser_ewc = subparsers.add_parser('JOINT', help='Joint training')
+
+
+    parser.print_help()
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
+def main():
+    args = parse_parameters()
+    experiment = Experiment(args)
+    experiment.run_experiment()
+
+if __name__ == '__main__':
+    main()
