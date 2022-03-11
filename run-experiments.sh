378bbb2b68de9421a22b9bd366d6be99382f7766
Gabriel Rębacz <gabrielrebacz@gmail.com>
2022-03-11 11:43:26 +0100

    experiment update

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,9 +1,9 @@
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 JOINT
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 NAIVE
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 ICARL --icarl_memory_size 2000
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 LWF --lwf_alpha 1 --lwf_temperature 2
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 EWC --ewc_lambda 2 --ewc_mode separate
-python src/main.py --dataset MNIST --epochs 5  --experiences 2  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 SI --si_lambda 0.1 --si_eps 1e-03
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 JOINT
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 NAIVE
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 ICARL --icarl_memory_size 2000
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 LWF --lwf_alpha 1 --lwf_temperature 2
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 EWC --ewc_lambda 2 --ewc_mode separate
+python src/main.py --dataset MNIST --epochs 5  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 2 SI --si_lambda 0.1 --si_eps 1e-03
 
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 JOINT
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 NAIVE
