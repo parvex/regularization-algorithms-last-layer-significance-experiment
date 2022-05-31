3f75b238040a50c3386723385c67bbc2dce00b8c
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2022-05-31 12:41:38 +0200

    last layer epochs fix ewc

--- run-experiments.sh
+++ run-experiments.sh
@@ -9,7 +9,7 @@ python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_r
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 NAIVE
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 ICARL --icarl_memory_size 2000
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 LWF --lwf_alpha 1 --lwf_temperature 2
-python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 70 EWC --ewc_lambda 2 --ewc_mode separate
+python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 EWC --ewc_lambda 2 --ewc_mode separate
 python src/main.py --dataset CIFAR100 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 --last_layer_epochs 20 SI --si_lambda 0.1 --si_eps 1e-03
 
 #python src/main.py --dataset CUB200 --epochs 70  --experiences 5  --learning_rate 1e-3 --batch_size 128 JOINT
